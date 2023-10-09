6d735302c3f5be43b2ba40b2d3c677d175cd3d2f
CodeSandbox <support@codesandbox.io>
2023-10-09 23:37:23 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./styles.css";
+import * as C from "./App.styles";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const App = () => {
+  return;
+  <C.Container>
+    <C.Map>
+    </C.Map>
+
+  </C.Container>;
+};
+
+export default App;
